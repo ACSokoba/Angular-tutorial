3ca1601ddecf91c2a854422b8a89ab1371443e05
unknown <sokobachristian@yahoo.fr>
2019-03-17 16:14:21 +0100

    Your Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mon-projet-angular';
+  isAuth = false;
+
+  
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'éteint'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'allumé'
+    }
+  ];
+
+  constructor() {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 2000
+    );
+  }
+
+  onAllumer() {
+    console.log("on allume tout");
+  }
+
 }
